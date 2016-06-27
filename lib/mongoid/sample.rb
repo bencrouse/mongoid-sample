53a87a4d1b910513d379fed9ea2edd9eb027a083
Ben Crouse <bcrouse@weblinc.com>
2016-06-26 20:25:21 -0400

    Initial implementation

--- lib/mongoid/sample.rb
+++ lib/mongoid/sample.rb
@@ -1,7 +1,26 @@
-require "mongoid/sample/version"
+require 'mongoid/sample/version'
+require 'mongoid'
+
+module Mongoid
+  class Criteria
+    def sample(n = 1)
+      indexes = (0..count - 1).sort_by { rand }.slice(0, n)
+
+      if n == 1
+        skip(indexes.first).first
+      else
+        indexes.map { |i| skip(i).first }
+      end
+    end
+  end
+end
 
 module Mongoid
   module Sample
-    # Your code goes here...
+    def sample(*args)
+      criteria.sample(*args)
+    end
   end
 end
+
+Mongoid::Document::ClassMethods.send(:include, Mongoid::Sample)
--- test/mongoid/sample_test.rb
+++ test/mongoid/sample_test.rb
@@ -1,11 +1,43 @@
 require 'test_helper'
 
 class Mongoid::SampleTest < Minitest::Test
+  class Foo
+    include Mongoid::Document
+    field :name, type: String
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Mongoid::Sample::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_providing_a_random_document_on_empty_collection
+    assert(Foo.all.sample.nil?)
+  end
+
+  def test_providing_a_random_document
+    Foo.create!
+    assert(Foo.all.sample.present?)
+  end
+
+  def test_providing_multiple_documents
+    5.times { Foo.create! }
+    result = Foo.all.sample(5)
+
+    assert(result.length == 5)
+    assert(result.all?(&:present?))
+  end
+
+  def test_sample_without_scope
+    5.times { Foo.create! }
+    result = Foo.sample(5)
+
+    assert(result.length == 5)
+    assert(result.all?(&:present?))
+  end
+
+  def test_sample_respecting_scope
+    Foo.create!(name: 'foo')
+    assert(Foo.where(name: 'bar').sample.nil?)
+    assert(Foo.where(name: 'foo').sample.present?)
   end
 end
